175649d0cd06e306c944397d3c5c8c04e1dbd0bf
Jiri Freyvald <jiri.freyvald@gmail.com>
2023-09-23 01:39:58 +0200

    real sites data

--- src/sites.js
+++ src/sites.js
@@ -1,11 +1,44 @@
 const config = require('./config')
 const axios = require('axios')
 
+
+const distance = (lat1,lon1,lat2,lon2) => {
+    const R = 6371e3; // metres
+    const psi1 = lat1 * Math.PI/180; // φ, λ in radians
+    const psi2 = lat2 * Math.PI/180;
+    const deltaPsi = (lat2-lat1) * Math.PI/180;
+    const deltaLambda = (lon2-lon1) * Math.PI/180;
+    
+    const a = Math.sin(deltaPsi/2) * Math.sin(deltaPsi/2) +
+              Math.cos(psi1) * Math.cos(psi2) *
+              Math.sin(deltaLambda/2) * Math.sin(deltaLambda/2);
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    
+    return R * c / 1000; // in km
+    
+}
+
 const accountDetails = async (req,res) => {
     const sitesResponse = await axios.get("https://www.paragliding-mapa.cz/api/v0.1/launch")
+
+    sitesResponse.data.data.forEach((el) => {
+        //BRNO: const d = distance(49.1826582,16.6316407,el.latitude, el.longitude)
+        const d = distance(50.0915,16.2631,el.latitude, el.longitude)
+         
+        if (d<50) {
+            console.log(el.name, el.latitude, el.longitude,d)
+        }
+        
+    })
     return res.json(sitesResponse.data.data[0])
 }
 
 module.exports = {
     accountDetails
 }
+
+
+//Němčany 49.1562 16.9249
+//16.6316407&y=49.1826582
+//BRNO x=16.9249&y=49.1562
+//DOUDLEBY x=16.2510433&y=50.1072265&z=16
