5960870270dfd7dc4cf91c4d5e6945da193d5f03
Jiri Freyvald <jiri.freyvald@gmail.com>
2023-09-23 00:44:11 +0200

    without cors

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-const cors = require("cors")
+//const cors = require("cors")
 
 const express = require('express')
 
 const app = express()
 
-app.use(cors())
+//app.use(cors())
 app.use(express.urlencoded( {extended: true} ))
 app.use(express.json())
 
