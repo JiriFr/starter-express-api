34126eafc703ec94fbdedd9cd2ed8eaed04266c2
Jiri Freyvald <jiri.freyvald@gmail.com>
2023-09-23 01:07:58 +0200

    test v3

--- src/sites.js
+++ src/sites.js
@@ -2,7 +2,7 @@ const config = require('./config')
 const axios = require('axios')
 
 const accountDetails = (req,res) => {
-    let data = {test: "Test1"}
+    let data = {test: "Test v3"}
     //return res.status(200).json(data)
     return res.json(data)
 }
