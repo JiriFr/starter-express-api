f469fe1b2448d3b2d3cd49e504244628485c07af
Jiri Freyvald <jiri.freyvald@gmail.com>
2023-09-23 01:15:06 +0200

    axios+paragliding mapa

--- src/sites.js
+++ src/sites.js
@@ -1,10 +1,9 @@
 const config = require('./config')
 const axios = require('axios')
 
-const accountDetails = (req,res) => {
-    let data = {test: "Test v3"}
-    //return res.status(200).json(data)
-    return res.json(data)
+const accountDetails = async (req,res) => {
+    const sitesResponse = await axios.get("https://www.paragliding-mapa.cz/api/v0.1/launch")
+    return res.json(sitesResponse.data.data[0])
 }
 
 module.exports = {
